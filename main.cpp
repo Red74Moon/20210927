c5003c14172ac2b1e9483cf83b1ca3decf74bfbe
Red74Moon <fedoormaster@gmail.com>
2021-09-27 09:27:21 +0900

    구구단 출력 추가

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	// 입력받은 구구단 출력
 
-	int num, j;
+	int num,i, j;
 	cout << "출력할 구구단을 적으세요(1~10) : ";
 	cin >> num;
 
@@ -13,27 +13,21 @@ int main()
 	{
 		cout << num << " * " << j << " = " << num * j << endl;
 	}
+	cout << endl << endl;
+
+	// 구구단 출력
+
+	for (i = 1; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			cout << i << " * " << j << " = " << i * j <<endl;
+		}
+		cout << endl;
+	}
+
+
+
+	return 0;
+
 }
-
-
-
-
-
-
-
-
-
-//	// 구구단 출력
-//
-//	int i, j;
-//
-//	for (i = 1; i < 10; i++)
-//	{
-//		for (j = 1; j < 10; j++)
-//		{
-//			cout << i << " * " << j << " = " << i*j <<endl;
-//		}
-//		cout << endl;
-//	}
-//	return 0;
-//}

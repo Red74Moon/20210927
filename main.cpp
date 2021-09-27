af65ecefe03de87abc748a9ad509df636f782c7d
Red74Moon <fedoormaster@gmail.com>
2021-09-27 13:18:32 +0900

    선생님 도움 코드

--- main.cpp
+++ main.cpp
@@ -9,28 +9,31 @@ int main()
 	// 80 - 70 C
 	// 70 - 60 D
 	// 60 - F
-	
+	// 선생님 도움 코드
 	int Score = 0;
+	char Grade = 'F';
 Restart:
 
 	cout << "성적을 입력하세요 (0~100) : ";
 	cin >> Score;
 
+	//성적처리
+
 	if (Score <= 100 && Score >= 90 )
 	{
-		cout << "성적은 " << " A " << " 입니다." << endl;
+		Grade = 'A';
 	}
 	else if (Score < 90 && Score >= 80)
 	{
-		cout << "성적은 " << " B " << " 입니다." << endl;
+		Grade = 'B';
 	}
 	else if (Score < 80 && Score >= 70)
 	{
-		cout << "성적은 " << " C " << " 입니다." << endl;
+		Grade = 'C';
 	}
 	else if (Score < 70 && Score >= 60)
 	{
-		cout << "성적은 " << " D " << " 입니다." << endl;
+		Grade = 'D';
 	}
 	else if (Score > 101 || Score < 0)
 	{
@@ -39,8 +42,9 @@ Restart:
 	}
 	else 
 	{
-		cout << "성적은 " << " F " << " 입니다." << endl;
+		Grade = 'F';
 	}
+	cout << "성적은 " << Grade << " 입니다." << endl;
 
 	return 0;
 

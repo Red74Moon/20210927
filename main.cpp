41ad9ebd2ba3dd388ff1c53737c347d6d64a405d
Red74Moon <fedoormaster@gmail.com>
2021-09-27 15:07:58 +0900

    수정

--- main.cpp
+++ main.cpp
@@ -17,15 +17,17 @@ int main()
 	// 난수 초기화 0 ~ RAND_MAX
 	srand((unsigned int)time(NULL));
 
+	//숫자 섞기 (shuffle)
 	for (int i = 0; i < 10; i++)
 	{
 		// 0 - 9
 		int R = rand() % 10;
 		// to do 
+		int R2 = rand() % 10;
 
-		int Temp = Number[i];
-		Number[i] = Number[R];
-		Number[R] = Temp;
+		int Temp = Number[R];
+		Number[R] = Number[R2];
+		Number[R2] = Temp;
 
 		cout << Number[R] << endl;
 	}

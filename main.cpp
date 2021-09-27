8c28336786591a524bef23067c83b1871e026638
Red74Moon <fedoormaster@gmail.com>
2021-09-27 09:23:55 +0900

    입력 받은 구구단 출력

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 입력받은 구구단 출력
+
+	int num, j;
+	cout << "출력할 구구단을 적으세요(1~10) : ";
+	cin >> num;
+
+	for (j = 1; j < 10; j++)
+	{
+		cout << num << " * " << j << " = " << num * j << endl;
+	}
+}
+
+
+
+
+
+
+
+
+
+//	// 구구단 출력
+//
+//	int i, j;
+//
+//	for (i = 1; i < 10; i++)
+//	{
+//		for (j = 1; j < 10; j++)
+//		{
+//			cout << i << " * " << j << " = " << i*j <<endl;
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}

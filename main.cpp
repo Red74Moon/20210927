80f2063b8539d93dcb0955c9f3b7d614d45dcd7e
Red74Moon <fedoormaster@gmail.com>
2021-09-27 12:43:31 +0900

    else if (Score > 101 || Score < 0) 	{ 		cout << "잘못 입력하였습니다." << endl; 	} 추가

--- main.cpp
+++ main.cpp
@@ -15,23 +15,27 @@ int main()
 	cout << "성적을 입력하세요 (0~100) : ";
 	cin >> Score;
 
-	if (Score >= 90 && Score <= 100 )
+	if (Score <= 100 && Score >= 90 )
 	{
 		cout << "성적은 " << " A " << " 입니다." << endl;
 	}
-	else if (Score >= 80 && Score < 90)
+	else if (Score < 90 && Score >= 80)
 	{
 		cout << "성적은 " << " B " << " 입니다." << endl;
 	}
-	else if (Score >= 70 && Score < 80)
+	else if (Score < 80 && Score >= 70)
 	{
 		cout << "성적은 " << " C " << " 입니다." << endl;
 	}
-	else if (Score >= 60 && Score < 70)
+	else if (Score < 70 && Score >= 60)
 	{
 		cout << "성적은 " << " D " << " 입니다." << endl;
 	}
-	else
+	else if (Score > 101 || Score < 0)
+	{
+		cout << "잘못 입력하였습니다." << endl;
+	}
+	else 
 	{
 		cout << "성적은 " << " F " << " 입니다." << endl;
 	}

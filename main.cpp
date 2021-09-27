9d9e4e4de5f9cfd86d5bfe7f2e824dcd609d799c
Red74Moon <fedoormaster@gmail.com>
2021-09-27 13:46:40 +0900

    배열 초기화 , 출력

--- main.cpp
+++ main.cpp
@@ -3,53 +3,16 @@ using namespace std;
 
 int main()
 {
-	// 성적을 입력 받아서
-	// 100 - 90 A
-	// 90 - 80 B
-	// 80 - 70 C
-	// 70 - 60 D
-	// 60 - F
-	// 선생님 도움 코드
+	int Number[10];
 
-	int Score = 0;
-	char Grade = 'F';
-
-	cout << "성적을 입력하세요 (0~100) : ";
-	cin >> Score;
-
-	//성적처리 switch-case
-
-	switch (Score /10)
+	for (int i = 0; i < 10; i++)
 	{
-		case 10: 
-		case 9:
-		{
-			Grade = 'A';
-			break;
-		}
-		case 8:
-		{
-			Grade = 'B';
-			break;
-		}
-		case 7:
-		{
-			Grade = 'C';
-			break;
-		}
-		case 6:
-		{
-			Grade = 'D';
-			break;
-		}
-		default:
-		{
-			Grade = 'F';
-			break;
-		}
+		Number[i] = i + 1;
 	}
-	cout << "성적은 " << Grade << " 입니다." << endl;
-
+	for (int i = 0; i < 10; i++)
+	{
+		cout << Number[i] << endl;
+	}
 	return 0;
 
 }
